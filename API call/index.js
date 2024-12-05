3aeae2e7b5933ac099b37b803fd571ff2ece4cc9
ramtechnow20 <bvhss20@gmail.com>
2024-12-05 13:53:37 +0530

    api

--- API call/index.js
+++ API call/index.js
@@ -4,9 +4,31 @@
 //     sampleData.then((apiValue) => apiValue.forEach(function(c){
 //         console.log(c);
 //         document.getElementsByClassName("container")[0].innerHTML += `<div class="box">
-//         <h1> ${c.name} </h1> 
+//         <h1> ${c.name} </h1>
 //         </div>`
 //     }))
 // };
 
 // apicall();
+
+function apicall() {
+  fetch(
+    "https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=20"
+  )
+    .then((item) => item.json())
+    .then((value) =>
+      value.forEach(function (ele) {
+        console.log(ele);
+        document.getElementsByClassName("container")[0].innerHTML += `
+        <div class="card">
+<img src="${ele.url}" alt="cat image" height="250" width="250px"/>
+</div>
+`;
+      })
+    );
+}
+function refersh(){
+    window,location.reload("refersh")
+}
+
+apicall();
