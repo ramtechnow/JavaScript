d55c8713a2b310dd4f3d4184613de1a4c958a4d2
ramtechnow20 <bvhss20@gmail.com>
2024-12-05 12:21:08 +0530

    new

---
+++ API call/index.js
@@ -0,0 +1,12 @@
+// function apicall() {
+//     let data = fetch("https://jsonplaceholder.typicode.com/users");
+//     let sampleData = data.then((item) => item.json());
+//     sampleData.then((apiValue) => apiValue.forEach(function(c){
+//         console.log(c);
+//         document.getElementsByClassName("container")[0].innerHTML += `<div class="box">
+//         <h1> ${c.name} </h1> 
+//         </div>`
+//     }))
+// };
+
+// apicall();
